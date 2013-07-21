b8dc48e7eff6a8f6602253eaaff321bfb216ea5c
SpamFusion <Spam@ZoneIRC.Net>
2013-07-21 13:05:41 -0500

    Create Spanish.php

---
+++ locale/Spanish.php
@@ -0,0 +1,43 @@
+<?php
+//Inicar Sesión
+$locale['zl_con'] = "Iniciar Sesión";
+$locale['zl_con_desc'] = "Nuevo Sistema De Conexión";
+
+//Bandeja De Entrada
+$locale['msg_con_0'] = "Tienes %u ";
+$locale['msg_con_1'] = "Mensaje";
+$locale['msg_con_2'] = "Mensajes";
+
+//Bienvenida A Los Invitados
+$locale['zl_con_in'] = "Bienvenido Invitado";
+
+//Mis Estadisticas
+$locale['zl_stats_titulo'] = "Mis Estadisticas";
+$locale['zl_con_est'] = "Mis Estadisticas";
+
+//Avatar
+$locale['zl_con_av'] = "Cambiar Avatar";
+
+//Estadisticas
+$locale['zl_con_st'] = "Mis Estadisticas";
+$locale['zl_con_avatar'] = "Mi Avatar";
+$locale['zl_con_nick'] = "Mi NiCk";
+$locale['zl_con_reg'] = "Fecha De Registro";
+$locale['zl_con_msg'] = "Mis Mensajes";
+$locale['zl_con_ip'] = "Mi IP";
+$locale['zl_con_id'] = "Mi ID";
+$locale['zl_con_pub'] = "Mensajes Publicos";
+$locale['zl_con_mini'] = "Mensajes MiniCharla";
+$locale['zl_con_foro'] = "Mensajes Del Foro";
+$locale['zl_con_art'] = "Mis Artículos";
+
+//Envios
+$locale['zl_con_des'] = "Descargas Enviadas";
+
+//Otra Información
+$locale['zl_con_miem'] = "Total De Miembros";
+$locale['zl_con_not'] = "Noticias";
+$locale['zl_con_m'] = "<font color='#FF0000'><b>Atención</b></font> Nuevo Panel: Conexión Panel 1.0<br />Usted podra descargar este panel en ".$settings['sitename']." Proximamente esperelo.";
+$locale['zl_con_v'] = "Validación En CSS3 (CSS Válido)";
+$locale['zl_con_e'] = "Registro De Errores <font color='#DFF2BF'><b>0</b></font>";
+?>
